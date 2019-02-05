928f070e88615cb2cdbe14a0a821fb06d78f75e4
Cody Russell <cody@jhu.edu>
2019-02-05 13:40:40 -0600

    Add Bar

---
+++ Bar/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Bar.Models;
+
+namespace Bar.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
